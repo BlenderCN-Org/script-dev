ddef599f134ee6859201e664452d1fb32a6caad8
Aditia A. Pratama <aditia.ap@gmail.com>
2013-03-29 09:29:23 +0700

    updated forget register n unregister in eof, my bad sorry :D

--- development/camera_title_safe.py
+++ development/camera_title_safe.py
@@ -232,6 +232,12 @@ def unregister():
         wm.keyconfigs.addon.keymaps.remove(km)
     del addon_keymaps[:]        
  
+def register():
+   bpy.utils.register_module(__name__)
+   
+def unregister():
+    bpy.utils.unregister_module(__name__)
+
 if __name__ == "__main__":
     register()
   
